953a6fb32e3783407dcd291d69e26eba3162e3b0
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2024-02-04 16:30:40 -0300

    Tecno3F Clase 1 - Introducción al internet de las cosas

---
+++ 1.EncendidoLED/sketch.ino
@@ -0,0 +1,16 @@
+int led = 16;
+
+void setup() {
+  
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500); // this speeds up the simulation
+
+}
